03e5a1c2169292e4e4d2c3f3e45138559f4e9ba6
jabooo188 <secretoadivina01@gmail.com>
2024-01-17 16:42:13 -0600

    **

--- ShoeShop/src/App.jsx
+++ ShoeShop/src/App.jsx
@@ -1,10 +1,25 @@
 
 import './App.css'
+import './components/footer.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Footer from './Footer'; // Ajusta la ruta según tu estructura de archivos
 
+function App (){
+
+    return (
+        <div>
+          {/* Otros componentes de tu aplicación */
+          ok
+          }
+          <Footer />
+        </div>
+      );
+
+}
 
 export default App
 
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
