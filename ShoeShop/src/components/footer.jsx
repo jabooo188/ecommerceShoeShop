4024ce2b2a91995f004bfcafce27a3266c079562
jmanueltorress <tjmanuel354@gmail.com>
2024-01-17 15:14:34 -0600

    add footer.jsx

---
+++ ShoeShop/src/components/footer.jsx
@@ -0,0 +1,15 @@
+// Pie de pagina Footer.jsx
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>&copy; Shoe shoes 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
